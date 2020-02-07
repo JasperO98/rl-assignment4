4311b1e8ba062d023a836f19a79cea4320f5128e
Wilco de Boer <deboer.wilco@gmail.com>
2020-02-07 14:40:25 +0100

    Fix input strings

--- task3.py
+++ task3.py
@@ -8,12 +8,12 @@ if __name__ == '__main__':
     while not board.game_over:
         board.print()
 
-        row = int(input("Row: "))
-        column = ord(input("Column: ")) - ord('a')
+        row = int(input('Row: '))
+        column = ord(input('Column: ')) - ord('a')
         while not board.exists((column, row)) or not board.is_empty((column, row)):
             print('Invalid Move')
-            row = int(input("Rij: "))
-            column = ord(input("Column: ")) - ord('a')
+            row = int(input('Row: '))
+            column = ord(input('Column: ')) - ord('a')
 
         board.place((column, row), colour)
         colour = board.get_opposite_color(colour)
