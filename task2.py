3ddcd4c66c2d9b09fa4e5991b6c7cbd763c2d373
Wilco de Boer <deboer.wilco@gmail.com>
2020-03-02 22:01:30 +0100

    Add more tests

--- task2.py
+++ task2.py
@@ -3,6 +3,12 @@ from hex.players.base import HexPlayerHuman
 from hex.players.montecarlo import HexPlayerMonteCarlo
 
 if __name__ == '__main__':
+    # test functionality
+    game = HexGame(2, HexPlayerMonteCarlo(30), HexPlayerMonteCarlo(1))
+    game.step(['tree'])
+    game.step([])
+    game.step(['tree'])
+
     # play a game
     game = HexGame(5, HexPlayerMonteCarlo(10), HexPlayerHuman())
     game.play()
