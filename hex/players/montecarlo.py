6a08303c4ed368743502dc905678223f70d6e5a9
Wilco de Boer <deboer.wilco@gmail.com>
2020-03-02 18:30:55 +0100

    Use directed graphs

--- hex/players/montecarlo.py
+++ hex/players/montecarlo.py
@@ -6,7 +6,7 @@ class HexPlayerMonteCarlo(HexPlayer):
     def __init__(self, timeout):
         super().__init__()
         self.timeout = timeout
-        self.tree = ig.Graph()
+        self.tree = ig.Graph(directed=True)
 
     def __str__(self):
         return 'MCTS\n(timeout ' + str(self.timeout) + ')'
