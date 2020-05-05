0d53dc1a3b3ddbcc121c75ec20b77a6368c6642e
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-05 17:29:06 +0200

    Add init and model folder

--- hex/players/selfplay.py
+++ hex/players/selfplay.py
@@ -2,7 +2,13 @@ from hex.players.base import HexPlayer
 
 
 class AlphaZeroSelfPlay(HexPlayer):
+    def __init__(self):
+        super().__init__()
+        # train model
+        # laad model
+
     def determine_move(self, board, renders):
+        # doe zet
         pass
 
     def __str__(self):
