d368a9db95a5f6565f80eff427b481f4a1ab539f
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-09 04:32:24 +0200

    No class variables

--- hex/alphazero.py
+++ hex/alphazero.py
@@ -60,31 +60,23 @@ class AlphaHexNN(NeuralNet):
     def build_model(self):
         input_boards = Input(shape=self.input)
         x_image = Reshape(target_shape=self.input + (1,))(input_boards)
-        h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels, 3, padding='same', use_bias=False)(x_image)))
-        h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels, 3, padding='same', use_bias=False)(h_conv1)))
-        h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels, 3, padding='valid', use_bias=False)(h_conv2)))
-        h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(self.num_channels, 3, padding='valid', use_bias=False)(h_conv3)))
+        h_conv1 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='same', use_bias=False)(x_image)))
+        h_conv2 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='same', use_bias=False)(h_conv1)))
+        h_conv3 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='valid', use_bias=False)(h_conv2)))
+        h_conv4 = Activation('relu')(BatchNormalization(axis=3)(Conv2D(512, 3, padding='valid', use_bias=False)(h_conv3)))
         h_conv4_flat = Flatten()(h_conv4)
-        s_fc1 = Dropout(self.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024, use_bias=False)(h_conv4_flat))))
-        s_fc2 = Dropout(self.dropout)(Activation('relu')(BatchNormalization(axis=1)(Dense(512, use_bias=False)(s_fc1))))
+        s_fc1 = Dropout(0.3)(Activation('relu')(BatchNormalization(axis=1)(Dense(1024, use_bias=False)(h_conv4_flat))))
+        s_fc2 = Dropout(0.3)(Activation('relu')(BatchNormalization(axis=1)(Dense(512, use_bias=False)(s_fc1))))
         pi = Dense(self.output, activation='softmax', name='pi')(s_fc2)
         v = Dense(1, activation='tanh', name='v')(s_fc2)
         model = Model(inputs=input_boards, outputs=[pi, v])
-        model.compile(loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=Adam(self.lr))
+        model.compile(loss=['categorical_crossentropy', 'mean_squared_error'], optimizer=Adam())
         return model
 
     def __init__(self, game):
         super().__init__(game)
-
         self.input = game.getBoardSize()
         self.output = game.getActionSize()
-
-        self.lr = 0.001
-        self.dropout = 0.3
-        self.epochs = 10
-        self.batch_size = 64
-        self.num_channels = 512
-
         self.model = self.build_model()
 
     def train(self, examples):
@@ -92,7 +84,7 @@ class AlphaHexNN(NeuralNet):
         input_boards = np.asarray(input_boards)
         target_pis = np.asarray(target_pis)
         target_vs = np.asarray(target_vs)
-        self.model.fit(x=input_boards, y=[target_pis, target_vs], batch_size=self.batch_size, epochs=self.epochs)
+        self.model.fit(x=input_boards, y=[target_pis, target_vs], batch_size=64, epochs=10)
 
     def predict(self, board):
         board = board[np.newaxis, :, :]
