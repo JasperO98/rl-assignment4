8f29ce2c5bbc8eafb1f22846d49018b2bead8538
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-10 16:14:45 +0200

    Aparte variabelen voor spelen en trainen

--- hex/players/selfplay.py
+++ hex/players/selfplay.py
@@ -7,14 +7,14 @@ from alphazero.MCTS import MCTS
 import numpy.random as npr
 
 
-class CoachArgs:
+class ArgsCoach:
     def __init__(self):
         self.numIters = 100
         self.maxlenOfQueue = 200000
         self.numEps = 100
         self.tempThreshold = 15
         self.numMCTSSims = 50
-        self.cpuct = 1
+        self.cpuct = 2
         self.numItersForTrainExamplesHistory = 20
         self.arenaCompare = 40
         self.updateThreshold = 0.5
@@ -37,31 +37,36 @@ class CoachArgs:
         ))
 
 
+class ArgsMCTS:
+    def __init__(self):
+        self.numMCTSSims = 100
+        self.cpuct = 1
+
+
 class AlphaZeroSelfPlay1(HexPlayer):
     NAME = 'player1'
 
     def __init__(self):
         super().__init__()
-        self.args = CoachArgs()
-        self.net = None
-        self.game = None
-        self.mcts = None
+        self.coach_args = ArgsCoach()
+        self.mcts_args = ArgsMCTS()
+        self.mcts_class = None
 
     def setup(self, size):
-        self.args.init(size, self.NAME)
-        self.game = AlphaHexGame(size)
-        self.net = AlphaHexNN(self.game)
-        self.mcts = MCTS(self.game, self.net, self.args)
+        self.coach_args.init(size, self.NAME)
+        game = AlphaHexGame(size)
+        net = AlphaHexNN(game)
 
-        if not self.net.exists_checkpoint(self.args.checkpoint, 'best.pth.tar'):
-            shutil.rmtree(self.args.checkpoint, True)
-            coach = Coach(self.game, self.net, self.args)
+        if not net.exists_checkpoint(self.coach_args.checkpoint, 'best.pth.tar'):
+            shutil.rmtree(self.coach_args.checkpoint, True)
+            coach = Coach(game, net, self.coach_args)
             coach.learn()
 
-        self.net.load_checkpoint(self.args.checkpoint, 'best.pth.tar')
+        net.load_checkpoint(self.coach_args.checkpoint, 'best.pth.tar')
+        self.mcts_class = MCTS(game, net, self.mcts_args)
 
     def determine_move(self, board, renders):
-        if self.mcts is None:
+        if self.mcts_class is None:
             self.setup(board.size)
 
         np_board = np.zeros((board.size, board.size))
@@ -71,7 +76,7 @@ class AlphaZeroSelfPlay1(HexPlayer):
             else:
                 np_board[key] = -1
 
-        pi = self.mcts.getActionProb(np_board, temp=0)
+        pi = self.mcts_class.getActionProb(np_board, temp=0)
         action = npr.choice(len(pi), p=pi)
         return divmod(action, board.size)
 
