19f37e9fe5c2ac38096e2d94d2f95f1d7c58afc8
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-10 21:47:44 +0200

    Betere task2

--- task2.py
+++ task2.py
@@ -1,8 +1,13 @@
 import setup
 from hex.game import HexGame
-from hex.players.base import HexPlayerHuman
+from hex.players.base import HexPlayerHuman, HexPlayerRandom
 from hex.players.selfplay import AlphaZeroSelfPlay1
 
 if __name__ == '__main__':
-    game = HexGame(5, AlphaZeroSelfPlay1(), HexPlayerHuman())
+    # ensure trained model exists
+    game = HexGame(7, AlphaZeroSelfPlay1(), HexPlayerRandom())
+    game.step([])
+
+    # play against trained model
+    game = HexGame(7, AlphaZeroSelfPlay1(), HexPlayerHuman())
     game.play()
