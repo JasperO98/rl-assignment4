20e229aba3f2ce9fe06d870d9253625dcce30b41
J. Ouwerkerk <j.ouwerkerk.2@umail.leidenuniv.nl>
2020-05-10 13:03:53 +0200

    change default parameters

--- task2.py
+++ task2.py
@@ -1,8 +1,8 @@
 import setup
 from hex.game import HexGame
 from hex.players.base import HexPlayerHuman
-from hex.players.selfplay import AlphaZeroSelfPlay2
+from hex.players.selfplay import AlphaZeroSelfPlay1
 
 if __name__ == '__main__':
-    game = HexGame(5, AlphaZeroSelfPlay2(), HexPlayerHuman())
+    game = HexGame(7, AlphaZeroSelfPlay1(), HexPlayerHuman())
     game.play()
