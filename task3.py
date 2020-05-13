6f961c4465b655a97de88e74d352c13100955787
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-13 17:58:50 +0200

    Add task3

---
+++ task3.py
@@ -0,0 +1,15 @@
+import setup
+from hex.tournament import HexTournament
+from hex.players.selfplay import AlphaZeroSelfPlay1
+from hex.players.base import HexPlayerRandom
+from hex.players.montecarlo import HexPlayerMonteCarloTime
+
+if __name__ == '__main__':
+    ht = HexTournament(5, (
+        HexPlayerRandom(),
+        AlphaZeroSelfPlay1(epochs=10, cp=5, episodes=50, threshold=0.5),
+        AlphaZeroSelfPlay1(epochs=10, cp=5, episodes=100, threshold=0.51),
+        HexPlayerMonteCarloTime(10, 1),
+    ))
+    ht.tournament()
+    ht.plot_elo()
