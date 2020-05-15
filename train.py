df483737ce89520cbb5d6ea2df851699be452a51
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-15 15:53:18 +0200

    train defaults

--- train.py
+++ train.py
@@ -2,6 +2,6 @@ import setup
 from hex.players.selfplay import AlphaZeroSelfPlay1, AlphaZeroSelfPlay2
 
 if __name__ == '__main__':
-    settings = dict(epochs=10, cp=5, episodes=50, threshold=0.5)
-    AlphaZeroSelfPlay1(**settings).setup(5)
-    AlphaZeroSelfPlay2(**settings).setup(5)
+    settings = dict(epochs=10, cp=5, episodes=100, threshold=0.51)
+    AlphaZeroSelfPlay1(**settings).setup(7)
+    AlphaZeroSelfPlay2(**settings).setup(7)
