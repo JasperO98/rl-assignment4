e999a753b166c859fba60a71e84288555923b709
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-17 13:38:30 +0200

    Revert "Vary Cp for UCT"
    This reverts commit 24179b216df053c201ddcc19e4afc998fce5caea.

--- task3.py
+++ task3.py
@@ -9,9 +9,9 @@ def irange(start, stop):
 
 if __name__ == '__main__':
     players = []
-    for i in irange(1, 20):
+    for i in irange(1, 10):
         players.append(AlphaZeroSelfPlay1())
-        players[-1].coach_args.cpuct = i
+        players[-1].coach_args.depth = i
 
     ht = HexTournament(5, players)
     ht.train()
