bf55605a6569e3a7f8eb42015425ef1cf9206703
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-18 22:05:29 +0200

    Setup 7x7 tournament

--- task3.py
+++ task3.py
@@ -1,22 +1,26 @@
 import setup
 from hex.tournament import HexTournament
-from hex.players.selfplay import AlphaZeroSelfPlay1
+from hex.players.montecarlo import HexPlayerMonteCarloTime
+from hex.players.alphabeta import HexPlayerEnhancedAB
 from hex.players.base import HexPlayerRandom
+from hex.players.selfplay import AlphaZeroSelfPlay1, AlphaZeroSelfPlay2
+from glob import glob
+from os.path import join, sep
 
 if __name__ == '__main__':
-    players = [HexPlayerRandom()]
+    players = [
+        HexPlayerRandom(),
+        HexPlayerMonteCarloTime(10, 1),
+        HexPlayerEnhancedAB(10, True),
+    ]
 
-    for hashed in (
-            -6518895601560197144,
-            7184645914559508968,
-            -8068969104488914968,
-            -2531536123151864856,
-            -3548057543391555352,
-            8337567419166355944,
-    ):
-        players.append(AlphaZeroSelfPlay1(hashed))
+    for path in glob(join('models', '7x7', '*', 'player1')):
+        players.append(AlphaZeroSelfPlay1(int(path.split(sep)[-2])))
+    for path in glob(join('models', '7x7', '*', 'player2')):
+        players.append(AlphaZeroSelfPlay2(int(path.split(sep)[-2])))
 
-    ht = HexTournament(5, players)
+    print(len(players), players)
+    ht = HexTournament(7, players)
     ht.train()
     ht.tournament()
     ht.plots()
