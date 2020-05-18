cbc511eb42a5f7ec6a06ba459a1c5bcf494d2814
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-18 04:34:41 +0200

    Vary `updateThreshold`

--- task3.py
+++ task3.py
@@ -9,9 +9,9 @@ def irange(start, stop, step=1):
 
 if __name__ == '__main__':
     players = []
-    for i in irange(1, 20):
+    for i in (0, 0.5, 0.51, 0.55, 0.6, 0.7):
         players.append(AlphaZeroSelfPlay1())
-        players[-1].coach_args.epochs = i
+        players[-1].coach_args.updateThreshold = i
     print(players)
 
     ht = HexTournament(5, players)
