61bf396c1da8657395150c9cd8dc9afb879fdbb3
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-18 04:23:20 +0200

    Vary epochs

--- task3.py
+++ task3.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     players = []
     for i in irange(1, 20):
         players.append(AlphaZeroSelfPlay1())
-        players[-1].coach_args.cpuct = i
+        players[-1].coach_args.epochs = i
     print(players)
 
     ht = HexTournament(5, players)
