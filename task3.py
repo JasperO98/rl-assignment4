df49c1503556d9054188e75612c569c1dc56ba1d
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-18 19:27:04 +0200

    Setup new tournament

--- task3.py
+++ task3.py
@@ -1,18 +1,20 @@
 import setup
 from hex.tournament import HexTournament
 from hex.players.selfplay import AlphaZeroSelfPlay1
-
-
-def irange(start, stop, step=1):
-    return range(start, stop + 1, step)
-
+from hex.players.base import HexPlayerRandom
 
 if __name__ == '__main__':
-    players = []
-    for i in (0, 0.5, 0.51, 0.55, 0.6, 0.7):
-        players.append(AlphaZeroSelfPlay1())
-        players[-1].coach_args.updateThreshold = i
-    print(players)
+    players = [HexPlayerRandom()]
+
+    for hashed in (
+            -6518895601560197144,
+            7184645914559508968,
+            -8068969104488914968,
+            -2531536123151864856,
+            -3548057543391555352,
+            8337567419166355944,
+    ):
+        players.append(AlphaZeroSelfPlay1(hashed))
 
     ht = HexTournament(5, players)
     ht.train()
