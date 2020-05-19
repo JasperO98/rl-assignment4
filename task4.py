4b160f8c1fc7f3de4aa1b0fd220a92ddb2cc53bd
Wilco de Boer <deboer.wilco@gmail.com>
2020-05-20 00:57:12 +0200

    Geen random player for task 4

--- task4.py
+++ task4.py
@@ -1,12 +1,11 @@
 import setup
 from hex.tournament import HexTournament
-from hex.players.base import HexPlayerRandom
 from hex.players.selfplay import AlphaZeroSelfPlay1, AlphaZeroSelfPlay2
 from glob import glob
 from os.path import join, sep
 
 if __name__ == '__main__':
-    players = [HexPlayerRandom()]
+    players = []
 
     for path in glob(join('models', '5x5', '*', 'player1')):
         players.append(AlphaZeroSelfPlay1(int(path.split(sep)[-2])))
